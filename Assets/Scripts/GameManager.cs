e57c4935fab2d94ffffb979a48bf17e70b7879fd
Rushhourz <sascha.hampl@outlook.com>
2020-08-26 12:39:33 +0200

    a

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+/// <summary> Manages the state of the whole application </summary>
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private string gameScene;
+
+    public void Play()
+    {
+        StartCoroutine(LoadScene(gameScene));
+    }
+
+    private IEnumerator LoadScene(string sceneName)
+    {
+        Debug.Log("Loading game!");
+        yield return new WaitForSeconds(14);
+        EditorSceneManager.LoadScene(sceneName);
+    }
+}
